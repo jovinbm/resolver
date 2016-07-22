21648ab62b2f43e821321b22626169476b6c7fac
Jovin Mwilanga <jovinbeda@hotmail.com>
2016-07-22 22:47:42 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// NODE
 var Resolver     = require('./lib/Resolver');
 var resolver     = new Resolver();
 exports.Resolver = resolver;
+
+// BROWSER
+var Resolver_browser     = require('./lib/Resolver_browser');
+var resolver_browser     = new Resolver_browser();
+exports.Resolver_browser = resolver_browser;
